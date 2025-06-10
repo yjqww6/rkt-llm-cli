5d9c34686631a46850c0fcd6498034f304ca3242
yjqww6 <343519265@qq.com>
2025-06-11 02:26:06 +0800

    ed

--- toolkit/ed.rkt
+++ toolkit/ed.rkt
@@ -1,6 +1,6 @@
 #lang racket/base
-(require racket/match racket/class racket/gui/base framework "../main.rkt")
-(provide edit input redo/input)
+(require racket/match racket/list racket/class racket/gui/base framework "../main.rkt")
+(provide edit input redo/input edit-history input-history)
 
 (define (get-content text)
   (let loop ([s (send text find-first-snip)]
@@ -34,8 +34,6 @@
                            (send ed last-position))]))]))
 
 (define (edit [init #f])
-  (define close-evt (make-semaphore))
-  (define ok-evt (make-semaphore))
   (define f (new dialog%
                  [label "Editor"]
                  [width 480]
@@ -79,3 +77,106 @@
            (current-history)))
        (unless (eq? new-history old)
          (current-history new-history)))]))
+
+(define (edit-history history)
+  (define f (new dialog%
+                 [label "Editor"]
+                 [width 480]
+                 [height 640]
+                 [style '(resize-border close-button)]))
+  (define h (new horizontal-pane% [parent f]))
+  (define on-select
+    (λ (l e)
+      (cond
+        [(send l get-selection)
+         =>
+         (λ (s)
+           (send c set-editor (hash-ref texts (send l get-data s))))])))
+  (define v (new vertical-pane% [parent h] [stretchable-width #f] [min-width 100]))
+  (define l (new list-box% [label ""] [choices '()] [parent v] [callback on-select]))
+  (define c (new editor-canvas% [parent h]))
+  (define r (new choice% [label "Role"] [choices '("user" "assistant")] [parent v]))
+  (define texts (make-weak-hasheq))
+  
+  (define k (new keymap%))
+  (keymap:setup-global k)
+  (add-editor-keymap-functions k)
+  (add-text-keymap-functions k)
+  ((current-text-keymap-initializer) k)
+  
+  (define (new-text! msg)
+    (define t (new text%))
+    (send t set-keymap k)
+    (send t set-max-undo-history 100)
+    (load-content t (Msg-content msg))
+    (hash-set! texts msg t))
+  
+  (define (adjust f)
+    (define n (send l get-number))
+    (define history
+      (for/list ([i (in-range (send l get-number))])
+        (send l get-data i)))
+    (define-values (new-history new-s) (f history (send l get-selection)))
+    (send l clear)
+    (for ([msg (in-list new-history)])
+      (send l append (Msg-role msg) msg))
+    (when new-s
+      (send l select new-s)
+      (on-select l #f)))
+  
+  (define (make-msg)
+    (define msg
+      (match (send r get-string-selection)
+        ["user" (make-user "")]
+        ["assistant" (make-assistant "")]))
+    (new-text! msg)
+    msg)
+  
+  (new button% [label "Insert Before"] [parent v]
+       [callback (λ (b e)
+                   (adjust (λ (h s)
+                             (cond
+                               [(not s) (values (cons (make-msg) h) 0)]
+                               [else
+                                (define-values (a b) (split-at h s))
+                                (values (append a (list (make-msg)) b) s)]))))])
+  
+  (new button% [label "Insert After"] [parent v]
+       [callback (λ (b e)
+                   (adjust (λ (h s)
+                             (cond
+                               [(not s) (values (append h (list (make-msg))) (length h))]
+                               [else
+                                (define-values (a b) (split-at h (+ s 1)))
+                                (values (append a (list (make-msg)) b) (+ s 1))]))))])
+  (new button% [label "Delete"] [parent v]
+       [callback (λ (b e)
+                   (define s (send l get-selection))
+                   (when s (send l delete s)))])
+
+  (define ok? #f)
+  (define-values (ok-button cancel-button)
+    (gui-utils:ok/cancel-buttons
+     f
+     (λ (b e) (set! ok? #t) (send f show #f))
+     (λ (b e) (send f show #f))
+     "OK"
+     "Cancel"))
+  
+  (for ([msg (in-list history)])
+    (define new-msg (struct-copy Msg msg))
+    (new-text! new-msg)
+    (send l append (Msg-role msg) new-msg))
+  (when (> (length history) 0)
+    (send l select (- (length history) 1))
+    (on-select l #f))
+  (send f show #t)
+  (and ok?
+       (for/list ([i (in-range (send l get-number))])
+         (define msg (send l get-data i))
+         (struct-copy Msg msg [content (get-content (hash-ref texts msg))]))))
+
+(define (input-history)
+  (cond
+    [(edit-history (current-history))
+     => current-history]))
